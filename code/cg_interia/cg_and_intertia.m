09dd159348b6b4362e2a9129454b502a04358cfb
FRAZ5094 <frasergmorrison@btinternet.com>
2021-11-15 21:22:44 +0000

    actually works now holy fuck

--- code/cg_interia/cg_and_intertia.m
+++ code/cg_interia/cg_and_intertia.m
@@ -4,36 +4,24 @@ close all
 format long g
 Data = readtable("aircraft_items.csv");
 
-mass=48000;
-cg.x=1.94;
-cg.y=3.06;
-cg.z=1.5;
+mass=36000;
+cg.x=2.75;
+cg.y=1.875;
+cg.z=1;
 
 %new values after Boss-Extrude2 removed
-cg_ans.x=2.62;
-cg_ans.y=1.51;
-cg_ans.z=1.04;
 
 
-I.xx=129812.5;
-I.xy=383.90;
-I.xz=35109.97;
+I.xx=60937.5;
+I.xy=16875;
+I.xz=0;
 I.yx=I.xy;
-I.yy=173711.08;
-I.yz=-487.03;
+I.yy=87750;
+I.yz=0;
 I.zx=I.xz;
 I.zy=I.yz;
-I.zz=119209.42;
+I.zz=124687.5;
 
-I_ans.xx=78560.43;
-I_ans.xy=547.69;
-I_ans.xz=-2587.30;
-I_ans.yx=I_ans.xy;
-I_ans.yy=65865.38;
-I_ans.yz=-195.13;
-I_ans.zx=I_ans.xz;
-I_ans.zy=I_ans.yz;
-I_ans.zz=78560.43;
 
 
 %calculating new center of mass
@@ -51,13 +39,21 @@ for i=1:1:height(Data)
     cg_sum.z=cg_sum.z - (Data{i,2}*Data{i,5});
 end
 
-cg.x=cg_sum.x/mass;
-cg.y=cg_sum.y/mass;
-cg.z=cg_sum.z/mass;
+cg_new.x=cg_sum.x/mass;
+cg_new.y=cg_sum.y/mass;
+cg_new.z=cg_sum.z/mass;
 
-cg.x=1;
-cg.y=2.5;
-cg.z=1.5;
+%find I at the new cg
+
+% - because we are taking away the mass
+I.xx = I.xx - mass*((cg.y-cg_new.y)^2 + (cg.z-cg_new.z)^2);
+I.yy = I.yy - mass*((cg.x-cg_new.x)^2 + (cg.z-cg_new.z)^2);
+I.zz = I.zz - mass*((cg.x-cg_new.x)^2 + (cg.y-cg_new.y)^2);
+
+I.xy= I.xy - mass*(cg.x-cg_new.x)*(cg.y-cg_new.y);
+I.yz= I.yz - mass*(cg.y-cg_new.y)*(cg.z-cg_new.z);
+I.xz= I.xz - mass*(cg.x-cg_new.x)*(cg.z-cg_new.z);
+
 
 for i=1:1:height(Data)
     %relative distances between objects cg and airplane cg
@@ -74,14 +70,19 @@ for i=1:1:height(Data)
     W=Data{i,7};
     H=Data{i,8};
     
-    delta=(m/12)*(W^2 + H^2) + m*((delta_y)^2 + (delta_z)^2);
-    I.xx= I.xx - delta;
+    
+    I.xx= I.xx - ((m/12)*(W^2 + H^2) + m*((delta_y)^2 + (delta_z)^2));
+    I.yy= I.yy - ((m/12)*(L^2 + H^2) + m*((delta_x)^2 + (delta_z)^2));
+    I.zz= I.zz - ((m/12)*(L^2 + W^2) + m*((delta_x)^2 + (delta_y)^2));
+    
+    I.xy= I.xy - m*delta_x*delta_y;
+    I.yz= I.yz - m*delta_y*delta_z;
+    I.xz= I.xz - m*delta_x*delta_z;
 end
 
 
-%I=[I.xx I.yx I.xz; I.yx I.yy I.yz; I.zx I.zy I.zz];
-%I_ans=[I_ans.xx I_ans.yx I_ans.xz; I_ans.yx I_ans.yy I_ans.yz; I_ans.zx I_ans.zy I_ans.zz];
+I=[I.xx I.xy I.xz; I.xy I.yy I.yz; I.xz I.yz I.zz]
 
 
-I.xx
+
 
