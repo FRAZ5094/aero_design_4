1b60d25686a5f698203823000517721e4f17ff4d
FRAZ5094 <frasergmorrison@btinternet.com>
2021-11-16 21:03:15 +0000

    ???

--- code/cg_interia/cg_and_intertia.m
+++ code/cg_interia/cg_and_intertia.m
@@ -2,15 +2,12 @@ clc
 clear
 close all
 format long g
-Data = readtable("aircraft_items.csv");
+Data = readtable("aircraft_items.csv"); %import data from csv
 
 mass=36000;
 cg.x=2.75;
 cg.y=1.875;
 cg.z=1;
-
-%new values after Boss-Extrude2 removed
-
 
 I.xx=60937.5;
 I.xy=16875;
@@ -23,17 +20,15 @@ I.zy=I.yz;
 I.zz=124687.5;
 
 
-
-%calculating new center of mass
+%calculating new center of mass pos
 
 cg_sum.x=mass*cg.x;
 cg_sum.y=mass*cg.y;
 cg_sum.z=mass*cg.z;
 
 for i=1:1:height(Data)
-    
-    %cg stuff
     mass=mass - Data{i,2};
+    
     cg_sum.x=cg_sum.x - (Data{i,2}*Data{i,3});
     cg_sum.y=cg_sum.y - (Data{i,2}*Data{i,4});
     cg_sum.z=cg_sum.z - (Data{i,2}*Data{i,5});
@@ -45,36 +40,37 @@ cg_new.z=cg_sum.z/mass;
 
 %find I at the new cg
 
-% - because we are taking away the mass
 I.xx = I.xx - mass*((cg.y-cg_new.y)^2 + (cg.z-cg_new.z)^2);
 I.yy = I.yy - mass*((cg.x-cg_new.x)^2 + (cg.z-cg_new.z)^2);
 I.zz = I.zz - mass*((cg.x-cg_new.x)^2 + (cg.y-cg_new.y)^2);
-
-I.xy= I.xy - mass*(cg.x-cg_new.x)*(cg.y-cg_new.y);
-I.yz= I.yz - mass*(cg.y-cg_new.y)*(cg.z-cg_new.z);
-I.xz= I.xz - mass*(cg.x-cg_new.x)*(cg.z-cg_new.z);
+I.xy = I.xy - mass*(cg.x-cg_new.x)*(cg.y-cg_new.y);
+I.yz = I.yz - mass*(cg.y-cg_new.y)*(cg.z-cg_new.z);
+I.xz = I.xz - mass*(cg.x-cg_new.x)*(cg.z-cg_new.z);
 
 
 for i=1:1:height(Data)
+    
     %relative distances between objects cg and airplane cg
     delta_x=cg.x-Data{i,3};
     delta_y=cg.y-Data{i,4};
     delta_z=cg.z-Data{i,5};
     
-    x_g=Data{i,3};
-    y_g=Data{i,4};
-    z_g=Data{i,5};
-    
+    %object values from data
     m=Data{i,2};
+    
+    %dimension along x axis
     L=Data{i,6};
+    
+    %dimension along y axis
     W=Data{i,7};
+    
+    %dimension along z axis
     H=Data{i,8};
     
-    
+    %calculate new I when object is removed
     I.xx= I.xx - ((m/12)*(W^2 + H^2) + m*((delta_y)^2 + (delta_z)^2));
     I.yy= I.yy - ((m/12)*(L^2 + H^2) + m*((delta_x)^2 + (delta_z)^2));
     I.zz= I.zz - ((m/12)*(L^2 + W^2) + m*((delta_x)^2 + (delta_y)^2));
-    
     I.xy= I.xy - m*delta_x*delta_y;
     I.yz= I.yz - m*delta_y*delta_z;
     I.xz= I.xz - m*delta_x*delta_z;
@@ -82,7 +78,3 @@ end
 
 
 I=[I.xx I.xy I.xz; I.xy I.yy I.yz; I.xz I.yz I.zz]
-
-
-
-
